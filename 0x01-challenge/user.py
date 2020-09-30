63a50ae026b8308771fdcb6badaf2a4ce467eca2
s-macias <1552@holbertonschool.com>
2020-09-30 03:19:03 +0000

    task 2

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-""" 
+"""
 User class
 """
 
-class User():
-    """ Documentation """
 
+class User():
+    """ User class """
     def __init__(self):
-        """ Documentation """
+        """ Initializes user clas """
         self.__email = None
+
+    @property
+    def email(self):
+        """ getter method for email """
+        return self.__email
 
     @email.setter
     def email(self, value):
-        """ Documentation """
+        """ setter method for email """
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
-    
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     u = User()
     u.email = "john@snow.com"
     print(u.email)
